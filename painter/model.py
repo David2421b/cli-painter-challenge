efd3c02f3e12fc26e34e34f242b4547c9a59bbf7
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-08-05 12:53:37 -0500

    point and circle class creations

--- painter/model.py
+++ painter/model.py
@@ -1,4 +1,17 @@
 import matplotlib.pyplot as plt
 
-class Point
-    def __init__(self):
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x: float = x
+        self.y: float = y
+
+
+class Circle:
+    def __init__(self, center: Point, radius: float):
+        self.center: Point = center
+        self.radius: float = radius
+
+    def area(self) -> float:
+        return 3.1416 * self.radius ** 2
+
